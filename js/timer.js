32ac5d35f04e1f64cbf610410332ccd3d6eedffc
Luís Ferro <lfaferro@live.com.pt>
2020-05-12 13:10:47 +0100

    quick breaks
    - guardar os valores custom para as sessões de trabalho/pausa de um utilizador para a sua próxima sessão
    - opção para dar reset aos valores custom para voltar ao default

--- js/timer.js
+++ js/timer.js
@@ -6,13 +6,31 @@ const session_end = document.getElementById('endsession');
 const work_custom = document.getElementById('work_custom');
 const break_custom = document.getElementById('break_custom');
 const session_custom = document.getElementById('session_custom');
+const quick_break = document.getElementById('quick_break');
+const quickbreak_custom = document.getElementById('quickbreak_custom');
+const reset_sessions = document.getElementById('reset_sessions');
 
 let countdown = -1;
 
-var time_default = 60 * 25;
-var time_default_break; 60 * 5;
+var time_default;
+if(localStorage.getItem("work_session_custom")) {
+    var storage_work_session = localStorage.getItem("work_session_custom");
+    time_default = storage_work_session * 60;
+    if(storage_work_session < 10) storage_work_session = "0" + storage_work_session;
+    display.innerHTML = storage_work_session + ":" + "00";
+}
+else time_default = 60 * 25;
+
+var time_default_break;
+if(localStorage.getItem("break_session_custom")) time_default_break = localStorage.getItem("break_session_custom") * 60;
+else time_default_break = 60 * 5;
+
+var time_default_quickbreak = 60 * 2;
 var time_left = time_default;
 var session = 0;
+
+
+console.log(localStorage.getItem("work_session_custom") * 60)
 
 function startTimer(duration) {
     if(countdown == -1) {
@@ -66,18 +84,47 @@ function breakTime() {
     startTimer(time_default_break);
 }
 
+function quickBreak() {
+    clearInterval(countdown);
+    countdown = -1;
+    session_desc.innerHTML = "Quick Break!";
+    startTimer(time_default_quickbreak);
+    session++;
+}
+
+quick_break.addEventListener("click", e => {
+    e.preventDefault();
+    quickBreak();
+})
+
 reset.addEventListener("click", e => {
     e.preventDefault();
     resetTimer();
 })
 
+reset_sessions.addEventListener("click", e => {
+    localStorage.setItem("work_session_custom", 25);
+    localStorage.setItem("break_session_custom", 5);
+    localStorage.setItem("quickbreak_session_custom", 2);
+    window.location.reload();
+})
+
 session_custom.addEventListener("click", e => {
-    if(work_custom.value)
+    if(work_custom.value) {
+        localStorage.setItem("work_session_custom", work_custom.value);
         time_default = work_custom.value * 60;
+    }
     else time_default = 25 * 60;
-    if(break_custom.value)
+    if(break_custom.value) {
+        localStorage.setItem("break_session_custom", break_custom.value);
         time_default_break = break_custom.value * 60;
+    }
     else time_default_break = 5 * 60;
+    if(quickbreak_custom.value) {
+        localStorage.setItem("quickbreak_session_custom", quickbreak_custom.value);
+        time_default_quickbreak = quickbreak_custom.value * 60;
+    }
+    else time_default_quickbreak = 2 * 60;
     $(".close").click();
     session = 0;
     resetTimer();
